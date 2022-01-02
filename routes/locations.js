c95fe7d0f5afa803e3df3f66a4c75317fba3d852
sencadas <joaosencadas@ipvc.pt>
2022-01-02 01:54:27 +0000

    locations

--- routes/locations.js
+++ routes/locations.js
@@ -4,12 +4,25 @@ const Location = require("../models/Location");
 const utils = require("../utils/utils");
 const auth = require("../middleware/auth");
 
-router.get("/", async (req, res) => {
-  try {
-    const locations = await Location.find();
-    res.json(locations);
-  } catch (err) {
-    res.json({ message: err });
+router.get("/", auth.verifyToken, auth.verifyRole("any"), async (req, res) => {
+  const id = req.query.id;
+
+  if (id) {
+    await Location.findOne({ id })
+      .then((result) => {
+        res.json({ isSuccess: true, data: result });
+      })
+      .catch((err) => {
+        res.json({ isSuccess: false, data: "ID não encontrado" });
+      });
+  } else {
+    await Location.find()
+      .then((result) => {
+        res.json({ isSuccess: true, data: result });
+      })
+      .catch((err) => {
+        res.json({ isSuccess: false, data: "Ocorreu um erro" });
+      });
   }
 });
 
@@ -25,5 +38,62 @@ router.post("/createLocation", async (req, res) => {
     res.json(err);
   }
 });
+
+router.put(
+  "/updateLocation",
+  auth.verifyToken,
+  auth.verifyRole(["admin", "edit"]),
+  async (req, res) => {
+    const id = req.query.id;
+
+    const locationUpdated = {
+      location: req.body.location,
+    };
+
+    Location.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: locationUpdated,
+      },
+      {
+        //Caso não exista id insere
+        upsert: true,
+      }
+    )
+      .then((result) => {
+        if (result) {
+          res.json({ isSuccess: true, data: result });
+        } else {
+          res.json({ isSuccess: false, data: "ID não existe" });
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        res.json({ isSuccess: false, data: "Ocorreu um erro" });
+      });
+  }
+);
+
+router.delete(
+  "/deleteLocation",
+  auth.verifyToken,
+  auth.verifyRole(["admin", "edit"]),
+  async (req, res) => {
+    const id = req.query.id;
+
+    Location.deleteOne({ _id: id })
+      .then((result) => {
+        if (result) {
+          res.json({ isSuccess: true, data: result });
+        } else {
+          res.json({ isSuccess: false, data: "ID não existe" });
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json({ isSuccess: false, data: "Ocorreu um erro" });
+      });
+  }
+);
 
 module.exports = router;
